3eabe9707c8f56226702a2bef685da2b1fc809ad
Prem Kumar <74190219+Mpremk@users.noreply.github.com>
2024-03-11 11:57:47 +0530

    Create SAN.js

---
+++ SAN.js
@@ -0,0 +1,10 @@
+let isAbsent = true;
+
+   // Check if the student is absent
+if (isAbsent) {
+  // Display an alert message
+  alert("you are marked absent. Please take care of your attendance or else you will shortfall attendance from Giving exam.");
+} else {
+  // If the student is not absent
+  console.log("Student is present. No action required.");
+}
